8efec93b38183d2322f5ea3971f7f19bf748d40c
95caro <95caro.torres@gmail.com>
2019-06-26 15:41:57 -0400

    2) Como responsable de RRHH, debo poder aprobar/rechazar los pedidos de vacaciones de los empleados.**

--- ProyectoNomina2019/ProyectoNomina2019/Pedidos_vacaciones.xaml.cs
+++ ProyectoNomina2019/ProyectoNomina2019/Pedidos_vacaciones.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,6 +41,59 @@ namespace ProyectoNomina2019
         private void btnSalir_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void btnAprobar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgDatosVaciones.SelectedItem != null)
+            {
+                Vacaciones a = (Vacaciones)dgDatosVaciones.SelectedItem;
+
+                a.Estado = "Aprobado";
+
+
+                datos.Entry(a).State = System.Data.Entity.EntityState.Modified;
+                datos.SaveChanges();
+
+
+
+                CargarDatosGrilla();
+            }
+            else
+                MessageBox.Show("Debe seleccionar un registro de la grilla para Aprobar");
+        }
+
+        private void CargarDatosGrilla()
+        {
+            try
+            {
+                dgDatosVaciones.ItemsSource = datos.Vacaciones.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnRechazar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgDatosVaciones.SelectedItem != null)
+            {
+                Anticipo a = (Anticipo)dgDatosVaciones.SelectedItem;
+
+                a.Estado = "Aprobado";
+
+
+                datos.Entry(a).State = System.Data.Entity.EntityState.Modified;
+                datos.SaveChanges();
+
+
+
+                CargarDatosGrilla();
+            }
+            else
+                MessageBox.Show("Debe seleccionar un registro de la grilla para Aprobar");
         }
     }
 }
