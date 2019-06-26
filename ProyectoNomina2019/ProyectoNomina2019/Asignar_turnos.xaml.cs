e3b0887a15f86738f74ddf4d408fef6407be9f49
JonasRR97 <jonasmbayue@gmail.com>
2019-06-26 08:34:14 -0400

    Asignar turno empleado

--- ProyectoNomina2019/ProyectoNomina2019/Asignar_turnos.xaml.cs
+++ ProyectoNomina2019/ProyectoNomina2019/Asignar_turnos.xaml.cs
@@ -19,9 +19,61 @@ namespace ProyectoNomina2019
     /// </summary>
     public partial class Asignar_turnos : Window
     {
+        NominaEntities datos;
         public Asignar_turnos()
         {
             InitializeComponent();
+            datos = new NominaEntities();
+        }
+        private void CargarDatosGrilla()
+        {
+            try
+            {
+                dgTurnos.ItemsSource = datos.Turno.ToList();
+                dgEmpleados.ItemsSource = datos.Empleado.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CargarDatosGrilla();
+        }
+
+        private void BtnAsignarTurno_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (dgEmpleados.SelectedItem != null)
+                {
+                    if (dgTurnos.SelectedItem != null)
+                    {
+                        Empleado emp = (Empleado)dgEmpleados.SelectedItem;
+                        Turno t = (Turno)dgTurnos.SelectedItem;
+                        emp.Turno_Id = t.Id_Turno;
+
+                        datos.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+                        datos.SaveChanges();
+
+                        MessageBox.Show("Se ha asignado el turno al empleado exitosamente!");
+                        CargarDatosGrilla();
+
+                    }
+                    else
+                        MessageBox.Show("Debe seleccionar algun turno desde la grilla");
+                }
+                else
+                    MessageBox.Show("Debe seleccionar algun empleado desde la grilla");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
