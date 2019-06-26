8ec733b2d03a9b5efeecd39d2ba86f78bdb71a23
Anahí Aveiro Britos <ab_anahi@hotmail.com>
2019-06-25 22:03:29 -0400

    Ventana de Aumentos Salariales

--- ProyectoNomina2019/ProyectoNomina2019/Aumentos_salariales.xaml.cs
+++ ProyectoNomina2019/ProyectoNomina2019/Aumentos_salariales.xaml.cs
@@ -29,6 +29,73 @@ namespace ProyectoNomina2019
             usuarioID = userId;
         }
 
+        private void CargarDatosGrilla()
+        {
+            try
+            {
+                dgEmpleados.ItemsSource = datos.Empleado.ToList();
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            CargarDatosGrilla();
+
+        }
+
+        private void BtnActualizarSalario_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (dgEmpleados.SelectedItem != null)
+                {
+                    Empleado emp = (Empleado)dgEmpleados.SelectedItem;
+                    int nuevoSalario = int.Parse(txtNuevoSalario.Text);
+                    int anteriorSalario = emp.Salario_Basico;
+
+
+                    if (nuevoSalario > anteriorSalario)
+                    {
+                        Empleado_Salario_Historico historico = new Empleado_Salario_Historico();
+                        historico.Empleado_Id = emp.Id_Empleado;
+                        historico.Salario_Basico_Anterior = anteriorSalario;
+                        historico.Salario_Basico_Nuevo = nuevoSalario;
+                        historico.Fecha_Hora = System.DateTime.Now;
+
+                        //var usuarioId = (from u in datos.Usuario
+                        //                where u.Empleado_Id == emp.Id_Empleado
+                        //                select u.Id_Usuario).FirstOrDefault(); UsuarioId correspondiente al EmpleadoId
+
+                        historico.Usuario_Id = usuarioID; // UsuarioId correspondiente al usuario logueado
+                        emp.Salario_Basico = nuevoSalario;
+
+                        datos.Empleado_Salario_Historico.Add(historico);
+                        datos.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+                        datos.SaveChanges();
+                        MessageBox.Show("La operacion se completado con exito! (:");
+
+                        CargarDatosGrilla();
+                    }
+                    else
+                        MessageBox.Show("El nuevo salario debe ser mayor al anterior.");
+                    
+                }
+                else
+                    MessageBox.Show("Debe seleccionar algun empleado.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
     }
 }
