91b4dd0d9b9b94b2221e4e74311173c87d4e493a
95caro <95caro.torres@gmail.com>
2019-07-09 20:05:21 -0400

    Planilla

--- ProyectoNomina2019/ProyectoNomina2019/Habilitar_Planilla_De_Liquidaciones.xaml.cs
+++ ProyectoNomina2019/ProyectoNomina2019/Habilitar_Planilla_De_Liquidaciones.xaml.cs
@@ -28,7 +28,84 @@ namespace ProyectoNomina2019
             datos = new NominaEntities();
             usuarioID = userId;
         }
+        public void CargarGrilla()
+        {
+            try
+            {
+                dgLiquidaciones.ItemsSource = datos.Liquidacion_Mensual.ToList();
+                dgLiquidaciones.Columns[0].Visibility = Visibility.Hidden;
+                dgLiquidaciones.Columns[4].Visibility = Visibility.Hidden;
+                dgLiquidaciones.Columns[6].Visibility = Visibility.Hidden;
+                dgLiquidaciones.Columns[7].Visibility = Visibility.Hidden;
+                dgLiquidaciones.Columns[8].Visibility = Visibility.Hidden;
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CargarGrilla();
+            txtMes.Text = System.DateTime.Now.Month.ToString();
+            txtAnho.Text = System.DateTime.Now.Year.ToString();
+        }
+
+        private void BtnHabilitar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                if (txtMes.Text != null && txtAnho.Text != null)
+                {
+                    if (int.Parse(txtMes.Text) > 0 && int.Parse(txtMes.Text) < 13 && txtAnho.Text.Length == 4)
+                    {
+                        Liquidacion_Mensual lmNew = new Liquidacion_Mensual();
+                        short vMes = short.Parse(txtMes.Text);
+                        short vAnho = short.Parse(txtAnho.Text);
+
+                        lmNew.Mes = vMes;
+                        lmNew.Anho = vAnho;
+                        lmNew.Fecha_Generacion = System.DateTime.Now;
+                        lmNew.Usuario_Id = usuarioID;
+                        lmNew.Estado = "A";
+
+                        var lmdb = (from l in datos.Liquidacion_Mensual
+                                    where l.Anho == vAnho && l.Mes == vMes
+                                    select l).FirstOrDefault();
+
+                        if (lmdb != null)
+                        {
+                            MessageBox.Show("Ya existe una liquidacion con el mes y año ingresados");
+                            return;
+                        }
+                        else
+                        {
+                            datos.Liquidacion_Mensual.Add(lmNew);
+                            datos.SaveChanges();
+                            MessageBox.Show("Se ha habilitado una liquidacion exitosamente!");
+                            CargarGrilla();
+                        }
+
+                    }
+                    else
+                        MessageBox.Show("Uno de los campos tiene formato incorrecto");
+                }
+                else
+                    MessageBox.Show("Uno de los campos tiene formato incorrecto");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
