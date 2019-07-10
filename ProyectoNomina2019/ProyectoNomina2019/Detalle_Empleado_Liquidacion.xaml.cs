db288cbe1cad1e5ed131ac5b79e11f7a5bc2370c
Jose Florentin <joe_che55@hotmail.com>
2019-07-09 22:19:58 -0400

    Opcion de imprimir el detalle del empleado en formato PDF

--- ProyectoNomina2019/ProyectoNomina2019/Detalle_Empleado_Liquidacion.xaml.cs
+++ ProyectoNomina2019/ProyectoNomina2019/Detalle_Empleado_Liquidacion.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +15,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+
 
 namespace ProyectoNomina2019
 {
@@ -128,6 +134,207 @@ namespace ProyectoNomina2019
         private void BtnSalir_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        void ExportDataTableToPdf(DataTable dtblTable, String strPdfPath, string strHeader)
+        {
+            System.IO.FileStream fs = new FileStream(strPdfPath, FileMode.Create, FileAccess.Write, FileShare.None);
+            Document document = new Document();
+            document.SetPageSize(iTextSharp.text.PageSize.A4);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            document.Open();
+
+            //Report Header
+            BaseFont bfntHead = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fntHead = new Font(bfntHead, 16, 1);
+            iTextSharp.text.Paragraph prgHeading = new iTextSharp.text.Paragraph();
+            prgHeading.Alignment = Element.ALIGN_CENTER;
+            prgHeading.Add(new Chunk(strHeader.ToUpper(), fntHead));
+            document.Add(prgHeading);
+
+            //Author
+            iTextSharp.text.Paragraph prgAuthor = new iTextSharp.text.Paragraph();
+            BaseFont btnAuthor = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fntAuthor = new Font(btnAuthor, 8, 2);
+            prgAuthor.Alignment = Element.ALIGN_LEFT;
+            //prgAuthor.Add(new Chunk("Emitido por : Dotnet Mob", fntAuthor));
+            prgAuthor.Add(new Chunk("\nFecha de Emisión : " + DateTime.Now.ToShortDateString(), fntAuthor));
+            document.Add(prgAuthor);
+
+
+            //Empleado
+            iTextSharp.text.Paragraph prgEmp = new iTextSharp.text.Paragraph();
+            BaseFont btnEmp = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fntEmp = new Font(btnEmp, 8, 2);
+            prgEmp.Alignment = Element.ALIGN_LEFT;
+            prgEmp.Add(new Chunk("Empleado: " + empSelected.Nombres + " " + empSelected.Apellidos + "\n", fntAuthor));
+            prgEmp.Add(new Chunk("Fecha de liquidación: " + lmSelected.Mes + "/" + lmSelected.Anho, fntAuthor));
+            document.Add(prgEmp);
+
+            //Add a line seperation
+            iTextSharp.text.Paragraph p = new iTextSharp.text.Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 1)));
+            document.Add(p);
+
+            //Add line break
+            document.Add(new Chunk("\n", fntHead));
+
+            //Write the table
+            PdfPTable table = new PdfPTable(dtblTable.Columns.Count);
+            //Table header
+            BaseFont btnColumnHeader = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fntColumnHeader = new Font(btnColumnHeader, 10, 1/*, BaseColor.WHITE*/);
+            for (int i = 0; i < dtblTable.Columns.Count; i++)
+            {
+                PdfPCell cell = new PdfPCell();
+                cell.BackgroundColor = BaseColor.GRAY;
+                cell.AddElement(new Chunk(dtblTable.Columns[i].ColumnName.ToUpper(), fntColumnHeader));
+                table.AddCell(cell);
+            }
+            //table Data
+            for (int i = 0; i < dtblTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < dtblTable.Columns.Count; j++)
+                {
+                    table.AddCell(dtblTable.Rows[i][j].ToString());
+                }
+            }
+
+            document.Add(table);
+            document.Close();
+            writer.Close();
+            fs.Close();
+        }
+
+
+        DataTable MakeDataTable()
+        {
+            DataTable friend = new DataTable();
+            friend.Columns.Add("Concepto");
+            friend.Columns.Add("Ingresos");
+            friend.Columns.Add("Egresos");
+
+            var ingresos = (from d in datos.Liquidacion_Mensual_Detalle
+                            where d.Liquidacion_Id == lmSelected.Id_Liquidacion && d.Empleado_Id == empSelected.Id_Empleado
+                            && d.Monto >= 0
+                            select d).ToList();
+
+            var egresos = (from d in datos.Liquidacion_Mensual_Detalle
+                           where d.Liquidacion_Id == lmSelected.Id_Liquidacion && d.Empleado_Id == empSelected.Id_Empleado
+                           && d.Monto < 0
+                           select d).ToList();
+
+            int totalIngresos = 0;
+            int totalEgresos = 0;
+
+            foreach (Liquidacion_Mensual_Detalle lmd1 in ingresos)
+            {
+                DataRow row = friend.NewRow();
+                Concepto c = datos.Concepto.Find(lmd1.Concepto_Id);
+
+                row["Concepto"] = c.Descripcion;
+                row["Ingresos"] = lmd1.Monto;
+                totalIngresos += lmd1.Monto;
+
+                friend.Rows.Add(row);
+            }
+            foreach (Liquidacion_Mensual_Detalle lmd1 in egresos)
+            {
+                DataRow row = friend.NewRow();
+                Concepto c = datos.Concepto.Find(lmd1.Concepto_Id);
+
+                row["Concepto"] = c.Descripcion;
+                row["Egresos"] = lmd1.Monto;
+
+                totalEgresos += lmd1.Monto;
+                friend.Rows.Add(row);
+            }
+
+            totalIngresos += empSelected.Salario_Basico;
+
+            Liquidacion_Empleados_Salarios_Totales salTotal =
+               datos.Liquidacion_Empleados_Salarios_Totales.Find(lmSelected.Id_Liquidacion, empSelected.Id_Empleado);
+
+            try
+            {
+                DataRow row = friend.NewRow();
+                row["Concepto"] = "Salario Basico";
+                row["Ingresos"] = empSelected.Salario_Basico;
+                friend.Rows.Add(row);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+
+            try
+            {
+                DataRow row = friend.NewRow();
+                row["Concepto"] = " ";
+                friend.Rows.Add(row);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+
+            try
+            {
+                DataRow row = friend.NewRow();
+                row["Concepto"] = "Totales";
+                row["Ingresos"] = totalIngresos;
+                row["Egresos"] = totalEgresos;
+                friend.Rows.Add(row);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+
+            try
+            {
+
+                DataRow row = friend.NewRow();
+                row["Concepto"] = "Salario total a Cobrar";
+                row["Ingresos"] = salTotal.SalarioTotal;
+                friend.Rows.Add(row);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+
+            return friend;
+        }
+
+        private void btnImprimir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string mensaje = "Esta seguro que desea guardar este resumen en formato PDF?";
+                string titulo = "Imprimir resumen";
+                MessageBoxButton botones = MessageBoxButton.YesNo;
+
+                if (MessageBox.Show(mensaje, titulo, botones) == MessageBoxResult.Yes)
+                {
+                    DataTable dtbl = MakeDataTable();
+                    ExportDataTableToPdf(dtbl, System.IO.Path.GetFullPath(@"..\..\..\..\") + "Resumen_Liquidación_Mensual_" + empSelected.Nombres + "_" + empSelected.Apellidos + ".pdf", "Resumen de Liquidación Mensual");
+
+                    System.Diagnostics.Process.Start(System.IO.Path.GetFullPath(@"..\..\..\..\") + "Resumen_Liquidación_Mensual_" + empSelected.Nombres + "_" + empSelected.Apellidos + ".pdf");
+
+                }
+                else
+                {
+                    // solo se cierra el messageBox
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
         }
     }
 }
